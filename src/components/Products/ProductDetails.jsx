de0bba20d1210dcd6bd9b68319e2c53d5b4d1371
mukilan143 <mukilanmukilan174@gmail.com>
2025-07-21 23:06:05 +0530

    Best Seller Added

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -36,7 +36,7 @@ const ProductDetails = () => {
 
 
   return (
-    <section className='p-6 bg'>
+    <section className='p-1 md:p-4 lg:p-6 bg'>
         <div className='max-w-6xl mx-auto bg-white p-8 rounded-lg'>
             <div className='flex flex-col  w-full md:flex-row'>
               {/* Left Thumbnil */}
